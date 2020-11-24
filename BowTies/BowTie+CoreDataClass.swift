3c778bfe9fbb67ddad9f79073978be9732c8a913
Kenneth Jones <kennethjones1991@outlook.com>
2020-11-24 02:32:07 -0700

    Save and fetch an object successfully

---
+++ BowTies/BowTie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  BowTie+CoreDataClass.swift
+//  BowTies
+//
+//  Created by Kenneth Jones on 11/24/20.
+//  Copyright © 2020 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(BowTie)
+public class BowTie: NSManagedObject {
+
+}
